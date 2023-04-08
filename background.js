4d78d43a8d6509976ef1b946618d5505c15cb13d
pmercado <pablo@odacrem.com>
2023-04-08 07:14:26 -0400

    init

---
+++ background.js
@@ -0,0 +1,34 @@
+let path = "/admin/tournaments/90/raw"
+let url = ""
+chrome.runtime.onMessage.addListener(({message, data, sender}) => {
+  console.log(sender + ":" + message)
+  if (sender == "popup") {
+    url = data.url + path
+    return
+  }
+  if (sender == "content" && message == "leaderboard") {
+    const form_data = new URLSearchParams();
+    const leaderboard = data.leaderboard
+    form_data.append('data', JSON.stringify(leaderboard));
+    console.log("fetching:" + url)
+    chrome.runtime.sendMessage({message:"fetchStart", sender:"background"})
+    fetch(url, {
+      method: 'POST',
+      body: form_data,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    .then(response => {
+      if (!response.ok && !response.redirected) {
+        throw new Error('Network response was not ok');
+      }
+      chrome.runtime.sendMessage({message:"fetchComplete", sender:"background"})
+      console.log("done");
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+});
+
