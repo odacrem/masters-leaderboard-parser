ed2105d845617f1cd0f5dfd0c9465e7add429932
pmercado <pablo@odacrem.com>
2023-04-08 20:07:18 -0400

    styles

--- content.js
+++ content.js
@@ -65,6 +65,7 @@ const parseLeaderboard = function() {
 
 Extension.stop = function(interval) {
 	Extension.button.textContent = "Start"
+	Extension.dot.style.backgroundColor = "red"
   Extension.status = "stopped"
   console.log("Extension url:" + Extension.url + ":" + "interval:" + Extension.interval + " stopped")
   clearInterval(Extension.interval)
@@ -86,6 +87,7 @@ Extension.start = function() {
 	let url = document.getElementById('url').value
 	Extension.url = url
 	Extension.button.textContent = "Stop"
+	Extension.dot.style.backgroundColor = "green"
   Extension.run()
   const intervalTime = 2 * 60 * 1000;
   let interval = setInterval(function() {
@@ -105,16 +107,26 @@ chrome.runtime.onMessage.addListener(({message, sender, data}) => {
   }
 	if (message == "fetchStart") {
 		Extension.button.setAttribute("disabled", true)
-		Extension.button.textContent = "updating..."
+		Extension.button.textContent = "Updating..."
+		Extension.dot.style.backgroundColor = "orange"
 	}
-	if (message == "fetchComplete") {
+	if (message == "fetchComplete" || message == "fetchError") {
 		Extension.button.removeAttribute("disabled")
 	  let content = Extension.status == "running" ? "Stop" : "Start"
 		Extension.button.textContent = content
+		Extension.dot.style.backgroundColor = Extension.status == "running" ? "green" : "red"
 	}
 })
 
 Extension.setup = function() {
+	const dot = document.createElement('span');
+	dot.style.height='10px';
+	dot.style.width='10px';
+	dot.style.backgroundColor='red';
+	dot.style.borderRadius='50%';
+	dot.style.marginLeft='10px';
+	dot.style.display='inline-block';
+
 	const div = document.createElement('div');
 	const input = document.createElement('input');
 	input.setAttribute("type","text")
@@ -123,6 +135,7 @@ Extension.setup = function() {
 	const button = document.createElement('button');
 	button.textContent = 'Start';
 	button.setAttribute("id", "button")
+	button.style.width='80px'
 	// add an event listener to the button
 	button.addEventListener('click', () => {
 		if (Extension.interval) {
@@ -133,11 +146,12 @@ Extension.setup = function() {
 	});
 	div.appendChild(input)
 	div.appendChild(button)
+	div.appendChild(dot)
 
 	// set the position and top/left CSS properties
 	div.style.position = 'fixed';
-	div.style.top = '10px';
-	div.style.left = '10px';
+	div.style.top = '0px';
+	div.style.left = '0px';
 	div.style.zIndex = 1000;
 	div.style.backgroundColor = "white"
 	div.style.padding = "10px"
@@ -147,6 +161,7 @@ Extension.setup = function() {
 	// append the button to the body of the page
 	document.body.appendChild(div);
 	Extension.button = button
+	Extension.dot = dot
 }
 
 Extension.setup()
